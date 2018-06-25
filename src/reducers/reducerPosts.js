02eb666c977339465c2bea5cc706833684f8b75d
Dvontre A Coleman <dvontrec@gmail.com>
2018-06-24 21:12:43 -0400

    End of course

--- src/reducers/reducerPosts.js
+++ src/reducers/reducerPosts.js
@@ -1,5 +1,5 @@
 import _ from 'lodash';
-import { FETCH_POSTS, FETCH_POST } from '../actions';
+import { FETCH_POSTS, FETCH_POST, DELETE_POST } from '../actions';
 
 export default function (state = {}, action)
 {
@@ -12,6 +12,9 @@ export default function (state = {}, action)
 		case FETCH_POSTS:
 			//used lodash to return an object that contains an array of posts as objects seperated by ID and content
 			return _.mapKeys(action.payload.data, 'id');
+		case DELETE_POST
+			//if state contains the deleted ID return new state with the key removed
+			return _.omit(state, action.payload);
 
 
 		default: 
